801f686fa6899321d932d1b4bfe0e2c2b3d47160
hang yang <hangyang@hangs-iMac.local>
2019-01-21 13:57:57 -0800

    Initial Commit

---
+++ CameraApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CameraApp
+//
+//  Created by hang yang on 1/21/19.
+//  Copyright © 2019 hang yang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
